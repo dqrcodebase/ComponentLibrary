a266cc0289cf56792d3df6108053f0ff52996596
dengqr <deng@qr.com>
2021-02-21 16:24:43 +0800

    push

--- giftShow/test.js
+++ giftShow/test.js
@@ -1,30 +1,42 @@
 
 (function (params) {
-    class style {
-
-    }
+    const showNum = 4
+    let showArr = []
+    let awaitArr = []
 
     // 公共方法
     class Together {
         constructor(params) {
             this.params = params
-            this.element = $(`<div></div>`)
+            this.type = params.type
+            this.user = params.user
+            this.name = params.name
+            this.giftId = params.giftId
+            this.giftType = params.giftType
+            this.element = $(`<div type="${params.type}" user="${params.user}"></div>`)
             this.time = null
-
-            this.remove()
         }
 
         remove() {
-            this.time = setTimeout((params) => {
+            this.time = setTimeout(() => {
+              console.log(showArr)
                 this.element.remove()
+                showArr.forEach((ele,index,arr) => {
+                  if(ele.giftId == this.giftId && this.user == ele.user){
+                    console.log(index)
+                    arr.splice(index,1)
+                  }
+                })
+                if(awaitArr.length > 0) {
+                  console.log(awaitArr)
+                  filtration(awaitArr.shift())
+                }
             }, this.params.time)
         }
 
-        clearTiem() {
+        clearTime() {
             clearTimeout(this.time)
-            this.time = setTimeout(() => {
-                this.element.remove()
-            }, this.params.time)
+            this.remove()
         }
     }
 
@@ -33,112 +45,292 @@
         constructor(params) {
             super(params)
             this.params = params
-            this.type = params.type
-            this.user = params.user
             this.create()
+            this.remove()
         }
         create() {
-            this.element.attr('type', this.type)
-            this.element.attr('user', this.user)
-            this.element.append('坐骑')
+            const ele = `${this.params.name}`
+            this.element.append(ele)
             const warp = $('#wrap')
             warp.append(this.element)
         }
+
     }
 
     // 普通礼物
     class Common extends Together {
-        create() {
-            this.element.prop('type', '1')
-            this.element.prop('user', '45255')
+      constructor(params) {
+            super(params)
+            this.params = params
+            this.num = params.num
+            this.create()
+            this.remove()
 
-            const ele = `<div type="1" user="45255">ele</div>`
+        }
+        create() {
+            const ele = `<span>${this.params.name}</span>
+                        <span class="num">${this.params.num}</span>
+                        <span>${this.params.giftType}</span>`
             this.element.append(ele)
             const warp = $('#wrap')
+            this.element.clearTime = this.clearTime
             warp.append(this.element)
+        }
+        changeNum(params) {
+          this.num += params.num
+          this.element.children('.num').eq(0).text(this.num)
         }
     }
 
     // 会员礼物
     class VIP extends Together {
+      constructor(params) {
+            super(params)
+            this.params = params
+            this.num = params.num
+            this.create()
+            this.remove()
+
+        }
         create() {
-            this.element.append('坐骑')
+            const ele = `<span>${this.params.name}</span>
+                        <span class="num">${this.params.num}</span>
+                        <span>${this.params.giftType}</span>`
+            this.element.append(ele)
             const warp = $('#wrap')
             warp.append(this.element)
+        }
+        changeNum(params) {
+          this.num += params.num
+          this.element.children('.num').eq(0).text(this.num)
         }
     }
 
     // 豪华礼物
     class Luxury extends Together {
+      constructor(params) {
+            super(params)
+            this.params = params
+            this.num = params.num
+            this.create()
+            this.remove()
+
+        }
         create() {
-            this.element.append('坐骑')
+            const ele = `<span>${this.params.name}</span>
+                        <span class="num">${this.params.num}</span>
+                        <span>${this.params.giftType}</span>`
+            this.element.append(ele)
             const warp = $('#wrap')
             warp.append(this.element)
+        }
+        changeNum(params) {
+          this.num += params.num
+          this.element.children('.num').eq(0).text(this.num)
         }
     }
 
     // 判断是否连击
     function isDouble(params) {
-        const child = $('#wrap').children()
-        let doubleEle = null
-        child.each(function (index, domEle) {
-            const type = $(domEle).attr('type')
-            const user = $(domEle).attr('user')
-            if (params.type == type && params.user == user) {
-                doubleEle = domEle
+      let doubleEle = null
+      showArr.forEach((ele) => {
+        if (params.giftType == ele.giftType && params.type == ele.type && params.user == ele.user) {
+                ele.clearTime()
+                ele.changeNum(params)
+                doubleEle = ele
             }
-        })
+      })
         return doubleEle
     }
 
     // 筛选
     function filtration(params) {
+      
         if (isDouble(params)) {
-            const doubleEle = $(isDouble(params))
-            doubleEle.text('doubleEle')
-        } else {
+          return
+        } else if(showArr.length < showNum){
+            
             switch(params.type){
                 case '1': // 坐骑
-                    new Mount(params)
+                    showArr.push(new Mount(params))
                     break
                 case '2': // 普通礼物
-                    new Common(params)
+                    showArr.push(new Common(params))
                     break
                 case '3': // vip礼物
-                    new VIP(params)
+                    showArr.push(new VIP(params))
                     break
                 case '4': // 豪华礼物
-                    new Luxury(params)
+                    showArr.push(new Luxury(params))
                     break
             }
+        }else{
+          awaitArr.push(params)
         }
     }
     window.filtration = filtration
 })()
 
-const mount = {
+const mount1 = {
     type: "1", 
     user: '45255', 
+    name:'mount',
+    giftType:'1',
+    giftId:'1',
     time: 6000
 }
 
-const common = {
+const mount2 = {
+    type: "1", 
+    user: '45255', 
+    name:'mount',
+    giftType:'2',
+    giftId:'2',
+    time: 6000
+}
+const mount3 = {
+    type: "1", 
+    user: '45255', 
+    name:'mount',
+    giftType:'3',
+    giftId:'3',
+    time: 6000
+}
+const mount4 = {
+    type: "1", 
+    user: '45255', 
+    name:'mount',
+    giftType:'4',
+    giftId:'4',
+    time: 6000
+}
+
+const common1 = {
     type: "2", 
     user: '45255', 
-    time: 3000
+    name:'common',
+    time: 7000,
+    giftType:'1',
+    giftId:'5',
+    num: 4
 }
 
-const vip = {
+const common2 = {
+    type: "2", 
+    user: '45255', 
+    name:'common',
+    time: 7000,
+    giftType:'2',
+    giftId:'6',
+    num: 4
+}
+
+const common3 = {
+    type: "2", 
+    user: '45255', 
+    name:'common',
+    time: 7000,
+    giftType:'3',
+    giftId:'7',
+    num: 4
+}
+
+const vip1 = {
     type: "3", 
     user: '45255', 
-    time: 4000
-}
-const luxury = {
-    type: "4", 
-    user: '45255', 
-    time: 8000
+    name:'vip',
+    time: 4000,
+    giftType:'1',
+    giftId:'8',
+    num: 5
 }
 
-$('button').click(function (params) {
-    filtration(mount)
+const vip2 = {
+    type: "3", 
+    user: '45255', 
+    name:'vip',
+    time: 4000,
+    giftType:'2',
+    giftId:'9',
+    num: 5
+}
+
+const vip3 = {
+    type: "3", 
+    user: '45255', 
+    name:'vip',
+    time: 4000,
+    giftType:'3',
+    giftId:'10',
+    num: 5
+}
+
+const luxury1 = {
+    type: "4", 
+    user: '45255', 
+    name:'luxury',
+    time: 8000,
+    giftType:'1',
+    giftId:'11',
+    num: 3
+}
+
+const luxury2 = {
+    type: "4", 
+    user: '45255', 
+    name:'luxury',
+    time: 8000,
+    giftType:'2',
+    giftId:'12',
+    num: 3
+}
+const luxury3 = {
+    type: "4", 
+    user: '45255', 
+    name:'luxury',
+    time: 8000,
+    giftType:'3',
+    giftId:'13',
+    num: 3
+}
+$('#mount1').click(function (params) {
+    filtration(mount1)
+})
+
+$('#mount2').click(function (params) {
+    filtration(mount2)
+})
+
+$('#mount3').click(function (params) {
+    filtration(mount3)
+})
+$('#mount4').click(function (params) {
+    filtration(mount4)
+})
+$('#common1').click(function (params) {
+    filtration(common1)
+})
+$('#common2').click(function (params) {
+    filtration(common2)
+})
+$('#common3').click(function (params) {
+    filtration(common3)
+})
+$('#vip1').click(function (params) {
+    filtration(vip1)
+})
+$('#vip2').click(function (params) {
+    filtration(vip2)
+})
+$('#vip3').click(function (params) {
+    filtration(vip3)
+})
+$('#luxury1').click(function (params) {
+    filtration(luxury1)
+})
+$('#luxury2').click(function (params) {
+    filtration(luxury2)
+})
+$('#luxury3').click(function (params) {
+    filtration(luxury3)
 })
